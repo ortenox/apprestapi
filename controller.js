a445517e9bf95216e5e4efb8a4eadf561d6303fd
Gema <gemaprameswara@gmail.com>
2023-05-22 10:32:58 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,17 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("aplikasi rest API berjalan")
+    response.ok("aplikasi rest API berjalan", res)
+};
+
+//menampilkan data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds) {
+        if (error){
+            connection.log(error);
+        }
+        else {
+            response.ok(rows,res);
+        }
+    });
 };
