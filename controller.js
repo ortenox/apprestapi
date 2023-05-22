f42ef9eb0e45623b9a3d8b60d611d01544b792be
Gema <gemaprameswara@gmail.com>
2023-05-22 14:15:15 +0700

    Berhasil hapus data

--- controller.js
+++ controller.js
@@ -66,3 +66,16 @@ exports.ubahmahasiswa = function(req, res) {
         }
     });
 };
+
+exports.hapusmahasiswa = function(req,res) {
+    var id = req.body.id_mahasiswa;
+
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function(error,rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok('Berhasil hapus data mahasiswa',res);
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -17,5 +17,8 @@ module.exports = function(app) {
 
     app.route('/ubah')
         .put(jsonku.ubahmahasiswa);
+
+    app.route('/hapus')
+        .delete(jsonku.hapusmahasiswa);
 }
 
