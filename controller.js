ff93bf9c7826b6d936b5112433f8ef1b48a473cd
Gema <gemaprameswara@gmail.com>
2023-05-22 13:55:52 +0700

    Menambahkan data

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("aplikasi rest API berjalan", res)
@@ -11,10 +12,40 @@ exports.index = function(req, res){
 exports.tampilsemuamahasiswa = function(req, res) {
     connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds) {
         if (error){
-            connection.log(error);
+            console.log(error);
         }
         else {
             response.ok(rows,res);
+        }
+    });
+};
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields){
+            if (error){
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//Menambhakan data mahasiswa
+exports.tambahmahasiswa = function(req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)', 
+    [nim,nama,jurusan],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok('Berhasil menambahkan data', res);
         }
     });
 };
--- routes.js
+++ routes.js
@@ -8,5 +8,11 @@ module.exports = function(app) {
 
     app.route('/tampil')
         .get(jsonku.tampilsemuamahasiswa);
+    
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
+
+    app.route('/tambah')
+        .post(jsonku.tambahmahasiswa);
 }
 
